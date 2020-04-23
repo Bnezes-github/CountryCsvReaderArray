eb5da3f1b5607666cc81670b68b3ee7c378f6ae8
BRUNO ALMEIDA <bruno.nezes@gmail.com>
2020-04-23 23:29:05 +0100

    Add project files.

---
+++ Country.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountryReader;
+
+namespace CountryReader
+{
+    class Country
+
+    {
+        //Read Only Bag of properties
+        public  string Name { get; }
+        public string Code { get; }
+        public string Region{ get; }
+        public int Population { get; }
+
+        //Constructor
+        public Country(string name, string code, string region, int population)
+        {
+            this.Name = name;
+            this.Code = code;
+            this.Region = region;
+            this.Population = population;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountryReader;
+
+namespace CountryReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @" C:\Users\bruno\source\repos\CountryReader\book1.csv";
+            CsvReader reader = new CsvReader(filePath);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+            foreach (Country country in countries)
+            {
+                Console.WriteLine($"{country.Name}: {country.Population}");
+            }
+            Console.WriteLine();
+        }
+        public void Listing()
+        {
+            List<string> daysOfTheWeek = new List<string>
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Saturday",
+                "Friday",
+                "Friday"
+            };
+
+            /* or
+            List<string> daysOfTheWeek = new List<string>();
+            daysOfTheWeek.Add("Monday");
+            daysOfTheWeek.Add("Monday");
+            daysOfTheWeek.Add("Monday");
+            daysOfTheWeek.Add("Monday");
+            daysOfTheWeek.Add("Friday");
+            daysOfTheWeek.Add("Friday"); */
+            
+       
+        }
+    }
+}
